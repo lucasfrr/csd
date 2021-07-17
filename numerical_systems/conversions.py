1bc917ec20c187fb0c2c12ffaf6bb529345333f4
Lucas Ferreira <lucasferreiraek@gmail.com>
2021-07-17 15:48:41 -0300

    Fix from_decinal function

--- numerical_systems/conversions.py
+++ numerical_systems/conversions.py
@@ -1,10 +1,10 @@
-def to_decimal(number: int, base: int):
+def to_decimal(number: str, base: int):
     """
     This function converts a given number is in binary, octal or hexadecimal base
     to decimal base.
 
     Parameters:
-        number (int): The value you want convert.
+        number (str): The value you want convert.
         base (int): The numerical base what you want convert.
 
     Returns:
@@ -14,47 +14,48 @@ def to_decimal(number: int, base: int):
     if not base == 2 or base == 8 or base == 16:
         raise ValueError(f"O valor {base} não é um valor de base numérica válido.")
 
-    number = str(number) if base != 16 else number
     hexadecimal_values = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
     result = 0
 
     if not base == 16:
         for symbol in number:
-            result += int(symbol) * (base ** number.index(symbol))
+            expoent = (len(number) - 1) - number.index(symbol)
+            result += int(symbol) * (base ** expoent)
     else:
         for symbol in number:
+            expoent = (len(number) - 1) - number.index(symbol)
             try:
-                result += int(symbol) * (base ** number.index(symbol))
+                result += int(symbol) * (base ** expoent)
             except TypeError:
-                result += hexadecimal_values[symbol] * (base ** number.index(symbol))
+                result += hexadecimal_values[symbol] * (base ** expoent)
 
     return result
 
-def from_decimal(number: int, base: int):
+def from_decimal(number: str, base: int):
     """
     This function converts a given number is in decimal base to a
     binary, octal or hexadecimal base.
 
     Parameters:
-        number (int): The value you want convert.
+        number (str): The value you want convert.
         base (int): The numerical base what you want convert.
 
     Returns:
-        int: A number is in numerical base what you wanted.
+        str: A number is in numerical base what you wanted.
     """
 
     if not base == 2 or base == 8 or base == 16:
         raise ValueError(f"O valor {base} não é um valor de base numérica válido.")
 
     hexadecimal_values = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
-    result = list()
+    result = ""
 
-    while number > base:
+    while number >= base:
         rest = str(number % base)
-        result.append(rest)
+        result += str(rest)
         number = number // base
 
-    result = "".join(item for item in result)
-    result + str(number)
+    result += str(number)
+    result = result[::-1]
 
-    return result if base == 16 else int(result)
+    return result
