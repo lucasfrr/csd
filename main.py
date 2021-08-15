aafdefec496d0eeba5ec35ed14025df210644e22
Gerson Jatobá <gersonmjl@gmail.com>
2021-08-15 17:18:33 -0300

    feat: binary_sum added to main menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from numerical_systems.menu import numerical_system_menu
 from unit_computer_memory.menu import unit_computer_memory_menu
 from logical_gates.menu import logical_gates_menu
+from binary_sum.menu import binary_sum_menu
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
         print("1 - CONVERSOR DE BASES")
         print("2 - CONVERSOR DE UNIDADE DE MEMÓRIA COMPUTACIONAL")
         print("3 - PORTAS LÓGICAS")
+        print("4 - SOMAS BINÁRIAS")
 
         choice = int(input("Digite a opção ou (0) para sair: "))
 
@@ -20,6 +22,10 @@ def main():
             unit_computer_memory_menu()
         elif choice == 3:
             logical_gates_menu()
+        elif choice == 4:
+            binary_sum_menu()
+        else:
+            print("Escolha inválida")
 
 
 if __name__ == "__main__":
